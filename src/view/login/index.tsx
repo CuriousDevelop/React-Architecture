e8f91ad7f01e97bb7d1e8cc0dc30678aa599685a
Tirthak Shah <tirthak.shah@tatvasoft.com>
2020-01-31 19:21:41 +0530

    Initial Commit

---
+++ src/view/login/index.tsx
@@ -0,0 +1,8 @@
+import { Form as ElForm } from 'element-react';
+import React, { Component } from 'react';
+
+export default class extends Component {
+    render() {
+        return <ElForm></ElForm>;
+    }
+}
